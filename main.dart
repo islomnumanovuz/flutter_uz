c90b50303a42629420c8473c906867dc0b256845
islomnumanovuz <islomnumanov.uz@gmail.com>
2023-02-23 09:30:21 +0300

    feat: Add variable

--- main.dart
+++ main.dart
@@ -3,4 +3,15 @@ void main() {
 
   // one line comment
   /* multiline comment */
+
+  // Variables
+  // letters, numbers, underscore, $ sign
+  var day = 'Monday';
+  var year = 2023;
+
+  print(day);
+
+  var num = 5;
+  num = 7;
+  print(num);
 }
